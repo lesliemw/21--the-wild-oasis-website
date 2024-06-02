848b9f8e468347ebbd8fe4f0ae1c85ab806dbe0c
lesliemw <l.marie1598@gmail.com>
2024-06-02 12:23:02 +0100

    update files

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,12 @@ import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
 export const metadata = { title: "The Wild Oasis" };
+import "@/app/_styles/globals.css";
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-blue-900 text-gray-50 min-h-screen ">
         <header>
           <Logo />
         </header>
