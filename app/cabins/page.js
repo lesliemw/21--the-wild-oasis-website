0600bb3ca45fcd9ecb590632ad1d6237b287f5a1
lesliemw <l.marie1598@gmail.com>
2024-06-02 12:05:40 +0100

    add files

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,19 +1,7 @@
-import Navigation from "../components/Navigation";
-import Counter from "../components/Counter";
-
 async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-
   return (
     <div>
       <h1>Cabin Page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter />
     </div>
   );
 }
