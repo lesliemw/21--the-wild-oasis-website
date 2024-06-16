ab840defa1f4c35d68be23cdd18bc508b36446ca
lesliemw <l.marie1598@gmail.com>
2024-06-16 14:25:50 +0100

    update files

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,13 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export const metadata = {
   title: {
     template: "%s  |  The Wild Oasis",
@@ -10,17 +17,18 @@ export const metadata = {
     "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.",
 };
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen ">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        {children}
-        <footer>&copy; lesliemw</footer>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 grid px-8 py-12 ">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
