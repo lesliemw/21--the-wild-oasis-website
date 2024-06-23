a61b8539a8d284bc3b947bfba1311b6a39c5ae77
lesliemw <l.marie1598@gmail.com>
2024-06-23 23:16:01 +0100

    add files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "labwvkenvddcehdsrknk.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/CabinImages/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
