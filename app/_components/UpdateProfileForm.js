181155bf4e2273d3274d66afe918d8e00f7a12a2
lesliemw <l.marie1598@gmail.com>
2024-08-28 22:43:08 +0100

    update server actions

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,18 +1,21 @@
 "use client";
 
-import { useState } from "react";
+import { updateGuest } from "../_lib/actions";
 
-function UpdateProfileForm({ children }) {
-  const [count, setCount] = useState();
-  // CHANGE
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
+function UpdateProfileForm({ children, guest }) {
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
+
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
+          name="fullName"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -21,6 +24,8 @@ function UpdateProfileForm({ children }) {
         <label>Email address</label>
         <input
           disabled
+          defaultValue={email}
+          name="email"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -40,6 +45,7 @@ function UpdateProfileForm({ children }) {
         <label htmlFor="nationalID">National ID number</label>
         <input
           name="nationalID"
+          defaultValue={nationalID}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
