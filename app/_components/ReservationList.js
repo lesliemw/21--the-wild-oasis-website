f8f984b7c25ff3635cbda3eee02200b4c8be6765
lesliemw <l.marie1598@gmail.com>
2024-11-01 18:00:44 +0000

    add reservation edit, useOptimistic

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,20 @@
+"use client";
+import ReservationCard from "./ReservationCard";
+import { useOptimistic } from "react";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    () => {}
+  );
+
+  return (
+    <ul className="space-y-6">
+      {bookings.map((booking) => (
+        <ReservationCard booking={booking} key={booking.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { updateGuest } from "../_lib/actions";
-import { useFormStatus } from "react-dom";
+import SubmitButton from "./SubmitButton";
 
 function UpdateProfileForm({ children, guest }) {
   const { fullName, email, nationality, nationalID, countryFlag } = guest;
@@ -50,21 +50,9 @@ function UpdateProfileForm({ children, guest }) {
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
-      <Button />
+      <SubmitButton pendingLabel="Updating...">Update Profile</SubmitButton>
       <div className="flex justify-end items-center gap-6"></div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending } = useFormStatus();
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
   );
 }
 
